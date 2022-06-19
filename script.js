bec47c8926f5d29f752b4aed8c54b5c3d09d79be
yams606 <dyancic@gmail.com>
2022-06-19 18:50:37 +0930

    grid expands with a range slider

--- script.js
+++ script.js
@@ -2,15 +2,40 @@
 let gridDiv = document.createElement("div");
 let sketchContainer = document.getElementById("sketch-container");
 gridDiv.className = "sketch-block";
-gridDiv.draggable = "false";
+let sketchBlock = document.getElementsByClassName("sketch-block");
+
 function loadGrid(n) {
     for (i = 0; i < (n*n); i++) {
         gridDiv.id = i;
         sketchContainer.appendChild(gridDiv.cloneNode(true));
     }
 }
+
 // Grids up on load
 document.onload = loadGrid(16);
+
+//slider or clickdown menu for grid size increase
+let sizeSlider = document.getElementById("size-slider");
+sizeSlider.value = "16";
+
+//changes the number in the html base on slider value
+//delete grid bring in new grid with change width and height of class="sketch-block" to 1/n % when changing grid size
+function changeValue(v) {
+    document.getElementById("size-value").innerHTML = v;
+    const blockRemove = document.querySelectorAll(".sketch-block");
+    blockRemove.forEach(box => { 
+        box.remove();
+    });
+    loadGrid(v);
+    let u = (1 / v)*100;
+    let uString = u.toString();
+    uString += "%";
+    console.log(uString);
+    Array.from(sketchBlock).forEach(element => {
+        element.style.width = uString;
+        element.style.height = uString;
+    })
+}
 
 //color change func
 function fillBlock(e) {
@@ -25,31 +50,27 @@ function fillBlock(e) {
 
 //selects the div by id when clicked and calls change func
 let blocks = document.querySelectorAll(".sketch-block");
-Array.from(blocks).forEach( element => {
+Array.from(blocks).forEach(element => {
     element.addEventListener("mousedown", clickEvent => {
         fillBlock(element.id);
     })
 });
 
+
+
 //mousedown toggles a mouseover listener
 let mouseIsDown = false
-document.body.addEventListener('mousedown', () => mouseIsDown = true);
-document.body.addEventListener('mouseup', () => mouseIsDown = false);
+document.body.addEventListener("mousedown", () => mouseIsDown = true);
+document.body.addEventListener("mouseup", () => mouseIsDown = false);
 Array.from(blocks).forEach( element => {
     element.addEventListener("mouseover", clickEvent => {
         if (mouseIsDown) { fillBlock(element.id); }
     })
 });
 
-//change width and height of class="sketchblock" to 1/n % when changing grid size
 
-
-//stops the drag and drop buggy 
-const dragDrop = document.querySelector('div')
-dragDrop.addEventListener('dragstart', (e) => {
-  e.preventDefault()
-})
-
-dragDrop.addEventListener('drop', (e) => {
-  e.preventDefault()
-})
+//stops the drag and drop bug
+gridDiv.draggable = "false";
+const dragDrop = document.querySelectorAll(".sketch-container", ".sketch-block", "header", "footer");
+dragDrop.addEventListener("dragstart", (e) => { e.preventDefault() })
+dragDrop.addEventListener("drop", (e) => { e.preventDefault() })
